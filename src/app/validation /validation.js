95a25cbcb71be28d238d3521f96697a520552941
AyeNyeinSan <aye2904@gmail.com>
2025-02-21 16:43:03 +0700

    Add: createForm validation schema is added

---
+++ src/app/validation /validation.js
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+export const CreateFormSchema = z.object({
+  CustomerName: z.string().min(1, { message: "Customer Name is required" }),
+  CustomerEmail: z.string().email({ message: "Invalid email format" }),
+  AgentId: z.number().min(1, { message: "Agent ID is required" }),
+
+  SupportRegionId: z.number(),
+
+  ManyChatId: z
+    .string()
+    .regex(/^\d+$/, { message: "ManyChat ID must be a numeric value" })
+    .transform(Number) // Converts it to a number
+    .refine((n) => n >= 1, { message: "ManyChat ID must be at least 1" }),
+
+  ContactLink: z.string().url({ message: "Invalid URL format" }),
+
+  Amount: z
+    .string()
+    .regex(/^\d+(\.\d{0,2})?$/, {
+      message: "Amount must be a valid number with up to 2 decimal places",
+    })
+    .transform(Number) // Convert it to a number after validation
+    .refine((n) => n >= 0.01, { message: "Amount must be greater than 0" }),
+
+  Month: z
+    .string()
+    .min(1, { message: "Month is required" })
+    .transform(Number) // Convert to number
+    .refine((n) => n >= 1 && n <= 12, {
+      message: "Month must be between 1 and 12",
+    }),
+
+  Notes: z.string().optional(),
+
+  WalletId: z.number(),
+
+  ScreenShots: z
+    .array(z.string())
+    .min(1, { message: "You need to provide a screenshot" }),
+});
