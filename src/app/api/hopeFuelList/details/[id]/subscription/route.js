5b9bcf559d990ea79d679202da8b335856a1b17f
AyeNyeinSan <56792505+aye-nyeinSan@users.noreply.github.com>
2025-02-27 23:16:34 +0700

    Update src/app/api/hopeFuelList/details/[id]/subscription/route.js
    Co-authored-by: Kaung Phyo Wai <53686958+kaungphyowai@users.noreply.github.com>

--- src/app/api/hopeFuelList/details/[id]/subscription/route.js
+++ src/app/api/hopeFuelList/details/[id]/subscription/route.js
@@ -75,7 +75,7 @@ export async function GET(req, { params }) {
         endDate.setDate(0); // Set to last day of the month
       }
 
-      return validDates;
+      return entries;
     });
 
     return NextResponse.json(
