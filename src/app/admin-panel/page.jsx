5319b1e79caa1116397b41492da85a08eae4cb5d
yanminthwin-i4b <yanminthwin@i4bsolutions.com>
2025-05-12 19:13:09 +0700

    Fix: Refactor admin tabs

---
+++ src/app/admin-panel/page.jsx
@@ -0,0 +1,5 @@
+import AdminPanelClient from "./pageClient";
+
+export default function AdminPanel() {
+  return <AdminPanelClient />;
+}
---
+++ src/app/admin-panel/pageClient.jsx
@@ -0,0 +1,62 @@
+"use client";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { useState } from "react";
+import FormOpenClosePage from "../formopenclose/page";
+import ExportCSVPage from "./csv/page";
+import Dashboard from "./dashboard/page";
+import ExchangeRates from "./exchange-rates/page";
+
+const tabSx = {
+  fontSize: "16px",
+  fontWeight: 600,
+  textTransform: "none",
+  px: 0,
+  mr: 2,
+  "&:last-of-type": {
+    mr: 0,
+  },
+};
+
+const tabs = [
+  { label: "Dashboard", component: <Dashboard /> },
+  { label: "Form Open/Close", component: <FormOpenClosePage /> },
+  { label: "Currency Exchange Rate", component: <ExchangeRates /> },
+  { label: "Export CSV", component: <ExportCSVPage /> },
+];
+
+export default function AdminPanelClient() {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  return (
+    <Box sx={{ px: 2 }}>
+      <Typography sx={{ fontSize: "34px", fontWeight: 600 }}>
+        Admin Panel
+      </Typography>
+
+      <Box sx={{ borderBottom: 2, borderColor: "#E2E8F0" }}>
+        <Tabs
+          value={tabIndex}
+          onChange={(event, newValue) => setTabIndex(newValue)}
+          aria-label="Admin Panel Tabs"
+          sx={{
+            "& .MuiTab-root": {
+              color: (theme) => theme.palette.neutral.grey900,
+            },
+            "& .Mui-selected": {
+              color: (theme) => theme.palette.primary.red500,
+            },
+            "& .MuiTabs-indicator": {
+              backgroundColor: (theme) => theme.palette.primary.red500,
+            },
+          }}
+        >
+          {tabs.map((tab) => (
+            <Tab key={tab.label} label={tab.label} sx={tabSx} />
+          ))}
+        </Tabs>
+      </Box>
+
+      {tabs[tabIndex].component}
+    </Box>
+  );
+}
