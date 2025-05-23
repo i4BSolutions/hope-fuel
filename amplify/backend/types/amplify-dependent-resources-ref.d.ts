3f20a3e675a0ea49d901d4fd50731bc8eff64528
yanminthwin-i4b <yanminthwin@i4bsolutions.com>
2025-05-24 02:55:45 +0700

    Update: pull staging env

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3hopefuelstaging9055d67b": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
