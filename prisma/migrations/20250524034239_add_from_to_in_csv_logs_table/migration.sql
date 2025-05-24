a87062c7ae5836339825283186cf8fe897918c6e
thumin-dev <thumin@i4bsolutions.com>
2025-05-24 10:43:25 +0700

    feat: add new column from to in csv export logs table

---
+++ prisma/migrations/20250524034239_add_from_to_in_csv_logs_table/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE `csvexporttransactionlogs` ADD COLUMN `FromDate` DATE NULL,
+    ADD COLUMN `ToDate` DATE NULL;
+
+-- AddForeignKey
+ALTER TABLE `Fundraiser_ContactLinks` ADD CONSTRAINT `fundraiser_contactlinks_ibfk_2` FOREIGN KEY (`PlatformID`) REFERENCES `Platform`(`PlatformID`) ON DELETE CASCADE ON UPDATE RESTRICT;
