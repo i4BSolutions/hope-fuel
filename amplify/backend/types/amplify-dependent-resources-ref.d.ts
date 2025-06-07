45a622ff5bf626cb6699f5a201a153e5141730f1
yanminthwin-i4b <yanminthwin@i4bsolutions.com>
2025-06-07 10:49:44 +0700

    HotFix: amplify push with config

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "hopefuel": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3hopefuelstorage85ce1c8f": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
